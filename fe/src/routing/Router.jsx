31b2560440d46d5c2c67c634b1e0a23056054d04
ElloRabyndra <Ellorabyndra123@gmail.com>
2026-05-29 22:01:16 +0700

    feat: add not found and on development pages

--- fe/src/routing/Router.jsx
+++ fe/src/routing/Router.jsx
@@ -1,12 +1,19 @@
 import { Route, Routes } from "react-router";
 import Home from "../pages/Home";
+import Games from "../pages/Games";
 import ColorPalettePreview from "../components/dev/ColorPalettePreview";
+import NotFound from "../pages/NotFound";
+import OnDevelopment from "../pages/OnDevelopment";
 
 const Router = () => {
   return (
     <Routes>
       <Route index element={<Home />} />
+      <Route path="/daftar-game" element={<Games />} />
+      <Route path="/informasi-rating" element={<OnDevelopment />} />
+      <Route path="/konsultasi-adiksi" element={<OnDevelopment />} />
       <Route path="/color-palette" element={<ColorPalettePreview />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
